5b038ba7154007b2653c518664d928443bdb0eb2
gabriel.brunichaki <gabriel.brunichaki@acad.pucrs.br>
2019-08-09 15:56:39 -0300

    frontend web done

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
+import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
@@ -7,47 +9,68 @@ import dislike from '../assets/dislike.svg';
 import './Main.css';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        }
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(targetId) {
+    await api.post(`/devs/${targetId}/likes`, null, {
+      headers: {
+        user: match.params.id,
+      }
+    });
+
+    setUsers(users.filter(user => user._id !== targetId));
+  };
+
+  async function handleDislike(targetId) {
+    await api.post(`/devs/${targetId}/dislikes`, null, {
+      headers: {
+        user: match.params.id,
+      }
+    });
+
+    setUsers(users.filter(user => user._id !== targetId));
+  }
+
   return (
     <div className="main-container">
-      <p>{match.params.id}</p>
-      <img src={logo} alt="Tindev" />
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
       <div className="devs">
-        <div className="dev-card">
-          <img src="https://avatars0.githubusercontent.com/u/28872143?v=4" alt="Gabriel Weich" className="dev-card-avatar" />
-          <span className="dev-card-name">Gabriel Weich</span>
-          <span className="dev-card-username">gabrielweich</span>
-          <p className="dev-card-bio">Software Engineering student</p>
-          <div className="dev-card-actions">
-            <button type="button">
-              <img src={like} alt="Like" />
-              <img src={dislike} alt="Disike" />
-            </button>
-          </div>
-        </div>
-        <div className="dev-card">
-          <img src="https://avatars0.githubusercontent.com/u/28872143?v=4" alt="Gabriel Weich" className="dev-card-avatar" />
-          <span className="dev-card-name">Gabriel Weich</span>
-          <span className="dev-card-username">gabrielweich</span>
-          <p className="dev-card-bio">Software Engineering student sddsds fdsf dsfdsfdfdf sdf df sdfsfsd fsdf sfdfsd sdf sdf sdfs sfddfdfs.</p>
-          <div className="dev-card-actions">
-            <button type="button">
-              <img src={like} alt="Like" />
-              <img src={dislike} alt="Disike" />
-            </button>
-          </div>
-        </div>
-        <div className="dev-card">
-          <img src="https://avatars0.githubusercontent.com/u/28872143?v=4" alt="Gabriel Weich" className="dev-card-avatar" />
-          <span className="dev-card-name">Gabriel Weich</span>
-          <span className="dev-card-username">gabrielweich</span>
-          <p className="dev-card-bio">Software Engineering student</p>
-          <div className="dev-card-actions">
-            <button type="button">
-              <img src={like} alt="Like" />
-              <img src={dislike} alt="Disike" />
-            </button>
-          </div>
-        </div>
+        {
+          users.length > 0 ? users.map(user => (
+            <div className="dev-card" key={user._id}>
+              <img className="dev-card-avatar" src={user.avatar} alt={user.name || user.user} />
+              <div className="dev-card-content">
+                <strong className="dev-card-name">{user.name || user.user}</strong>
+                <span className="dev-card-username">/{user.user}</span>
+                <p className="dev-card-bio">{user.bio}</p>
+              </div>
+              <div className="dev-card-actions">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Disike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </div>
+          )) : <div className="empty">Deu por hoje!</div>
+        }
       </div>
     </div>
   );
